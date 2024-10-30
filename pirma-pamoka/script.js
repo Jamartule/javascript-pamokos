7f677496f97dcb1a7919bc682db1096dfd46cfbc
jamartule <marta.zaldokaite@gmail.com>
2024-10-30 21:44:51 +0200

    Initial commit

---
+++ pirma-pamoka/script.js
@@ -0,0 +1,28 @@
+// log
+
+console.log(1);
+
+//   clear
+console.clear();
+
+console.log("Marta");
+// error
+console.error("Klaida");
+//   Math
+console.log(2 < 1);
+// info
+console.info("Informacija");
+
+//   table
+console.table(["apple", "banana", "orange"]);
+
+//   Warn
+console.warn("Įspėjimas");
+
+//   time padeda pamatuoti kiekk laiko uztrunka vykdyti koda
+console.time();
+console.timeEnd();
+
+//   kiekviena eilutė yra tiek laiko užtrunkama kiek reikia.
+
+// document.getElementById("heading").innerHTML = "Hello world";
